eaaa3ebb3a02e2d351ca56f9bf1fe6ce50fada59
Josh Moss <45637452+outrun7@users.noreply.github.com>
2020-05-13 09:01:56 -0400

    Update fixcopypaste.sh

--- fixcopypaste.sh
+++ fixcopypaste.sh
@@ -1,3 +1,4 @@
+#code by Austin Dean, stored here for ease of access. 
 #!/bin/bash
 processString=$(ps -ef | grep '[0-9][0-9]:[0-9][0-9]:[0-9][0-9] /usr/bin/vmtoolsd -n vmusr') # get process info for vmtoolsd
 tokens=( $processString ) # tokenize
